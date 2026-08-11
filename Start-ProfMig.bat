a2a56d13df07d03eeaa20167be45879f25572a58
scorpido74 <remcodekievit@gmail.com>
2026-08-11 13:59:44 +0200

    De locatie ook veranderd naar de ProfMig.ps1

--- Start-ProfMig.bat
+++ Start-ProfMig.bat
@@ -1,6 +1,6 @@
 @echo off
 title Profile Migration Tool
 
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0ProfileMigration.ps1"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0src\ProfMig.ps1"
 
 pause
